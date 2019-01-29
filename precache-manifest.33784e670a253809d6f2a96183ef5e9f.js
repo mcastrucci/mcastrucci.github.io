408450309d1595a13b816601190b6dc3c657405b
Castrucci <maximiliano.castrucci@igt.com>
2019-01-29 00:55:32 -0300

    initial draft

---
+++ precache-manifest.33784e670a253809d6f2a96183ef5e9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a4b62b3c82f7266be41020690eef303",
+    "url": "/static/media/star_wars_cover.6a4b62b3.PNG"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "11c829ff12cda64312c5",
+    "url": "/static/js/main.11c829ff.chunk.js"
+  },
+  {
+    "revision": "4b5549dba99713da3565",
+    "url": "/static/js/1.4b5549db.chunk.js"
+  },
+  {
+    "revision": "11c829ff12cda64312c5",
+    "url": "/static/css/main.d56df132.chunk.css"
+  },
+  {
+    "revision": "43994d4fbc9c817374ce7c2b67375833",
+    "url": "/index.html"
+  }
+];
